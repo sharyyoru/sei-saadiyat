8e02910713eda3278cc93491404e85da90e96caf
sharyyoru <sharyyoru@gmail.com>
2026-09-08 18:40:08 +0400

    Add page_viewed conversion event to OpenAI pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,6 +85,7 @@ export default function RootLayout({
             __html: `
               !function(w,d,s,u){if(w.oaiq)return;var q=function(){q.q.push(arguments)};q.q=[];w.oaiq=q;var j=d.createElement(s);j.async=1;j.src=u;var f=d.getElementsByTagName(s)[0];f.parentNode.insertBefore(j,f)}(window,document,"script","https://bzrcdn.openai.com/sdk/oaiq.min.js");
               oaiq("init",{pixelId:"LmFuTFjbfgedWqG8BM9SZz",debug:true});
+              oaiq("measure","page_viewed",{type:"contents"});
             `,
           }}
         />
